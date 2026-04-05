09e0b0dd384705bcce7e24a85e0f077a46a94c5c
danya2271 <dany2271@yandex.ru>
2026-04-05 22:53:44 +0500

    common-net: Reduce ConnIdle to 30 sec

--- common/net/net.go
+++ common/net/net.go
@@ -11,7 +11,7 @@ import (
 
 // defines the maximum time an idle TCP session can survive in the tunnel, so
 // it should be consistent across HTTP versions and with other transports.
-const ConnIdleTimeout = 300 * time.Second
+const ConnIdleTimeout = 30 * time.Second
 
 // consistent with quic-go
 const QuicgoH3KeepAlivePeriod = 10 * time.Second
